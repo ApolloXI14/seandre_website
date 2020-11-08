fb6ccceb7e7e27d9d7142dbe5ed3986b3cbb6241
Sean McHugh <sean.a.mchugh@gmail.com>
2020-11-08 15:59:05 -0500

    Made navbar a flexbox and centered it

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,18 +5,20 @@ class Navbar extends Component{
    render(){
       return(
          <div class="navbar">
-		  <a href="#home">Home</a>
-		  <a href="#news">News</a>
-		  <div class="dropdown">
-		    <button class="dropbtn">Dropdown
-		      <i class=""></i>
-		    </button>
-		    <div class="dropdown-content">
-		      <a href="#">Link 1</a>
-		      <a href="#">Link 2</a>
-		      <a href="#">Link 3</a>
-		    </div>
-		  </div>
+          <div><a href="#home">Home</a></div>
+		  <div><a href="#news">News</a></div>
+		  
+			  <div class="dropdown">
+			    <button class="dropbtn">Dropdown
+			      <i class=""></i>
+			    </button>
+			    <div class="dropdown-content">
+			      <a href="#">Link 1</a>
+			      <a href="#">Link 2</a>
+			      <a href="#">Link 3</a>
+			    </div>
+			  </div>
+		
 		</div>
       );
    }
