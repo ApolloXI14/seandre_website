a73ff7fee21a3698b17e0135dd53ce19551695b5
Sean McHugh <sean.a.mchugh@gmail.com>
2020-11-22 13:00:07 -0500

    Implemented switch and route

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar'
+import Poems from './components/poems'
+import { Route, Switch, withRouter } from 'react-router-dom';
+
+
 
 class App extends Component{
    render(){
@@ -8,6 +12,9 @@ class App extends Component{
          <div class="intro">
          	<Navbar />
             <h1>Hello World</h1>
+            <Switch>
+				<Route exact path="/poems" component={Poems} />
+			</Switch>
          </div>
       );
    }
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styles from '../less/Navbar.less'
-import { Switch, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class Navbar extends Component{
    render(){
@@ -14,13 +14,13 @@ class Navbar extends Component{
 			      <i class=""></i>
 			    </button>
 			    <div class="dropdown-content">
-		    		<Switch>
+		    		
 		    			<ul>
 				      		<Link to="/poems">Poems</Link>
 				      		<Link to="/">Short Stories</Link>
 				      		<Link to="/">Blog</Link>
 			      		</ul>
-			      	</Switch>
+			      	
 			    </div>
 			  </div>
 		
