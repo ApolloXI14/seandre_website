fed4bcbe15dd4258811f6f858e0a942ccd5dffa6
Sean McHugh <sean.a.mchugh@gmail.com>
2020-11-30 14:56:56 -0500

    Fixed <Slide> template errors to fix rendering; TODO: implement Slideshow JS

--- components/Slide.js
+++ components/Slide.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 class Slide extends Component{
    render(){
    	return (
-			<div class="mySlides fade">
-				    <div class="numbertext">{this.props.index} / {this.props.length}</div>
-				    	<img src={this.props.imgSrc} style="width:100%" />
-				    <div class="text">{this.props.caption}</div>
+			<div className="mySlides fade">
+				    <div className="numbertext">{this.props.index} / {this.props.length}</div>
+				    	<img id="currentImg" src={this.props.imgSrc} />
+				    <div className="text">{this.props.caption}</div>
 		  	</div>
 		  );
 	}
