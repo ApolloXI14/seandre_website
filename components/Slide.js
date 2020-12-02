4279ff8b0ac57b21e8d874f3b5e8ad9839668952
Sean McHugh <sean.a.mchugh@gmail.com>
2020-12-01 23:18:53 -0500

    Moved style display tag to root of <Slide> component

--- components/Slide.js
+++ components/Slide.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 class Slide extends Component{
    render(){
    	return (
-			<div className="fade">
+			<div className="fade" style={{display: this.props.display}}>
 				    <div className="numbertext">{this.props.index} / {this.props.length}</div>
-				    	<img id="currentImg" src={this.props.imgSrc} style={{display: this.props.display}} />
+				    	<img id="currentImg" src={this.props.imgSrc} />
 				    <div className="text">{this.props.caption}</div>
 		  	</div>
 		  );
