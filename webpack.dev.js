970cb94e320bf4a8dda936c5b97cbe2f57bbe0e5
Sean McHugh <sean.a.mchugh@gmail.com>
2020-12-05 13:55:10 -0500

    Updated babel-loader and webpack; made separate webpack files (common, dev, prod)

---
+++ webpack.dev.js
@@ -0,0 +1,13 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+	mode: 'development',
+	devtool: 'inline-source-map',
+	devServer: {
+		inline: true,
+	      port: 8001,
+	      historyApiFallback: true,
+	      hot: true
+	}
+});
