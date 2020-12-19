de398d05a835df3b3a2e5433f4593a689221b59d
Sean McHugh <sean.a.mchugh@gmail.com>
2020-12-19 02:10:59 -0500

    Made "sanitizeFileName" function to remove non-alphanum characters

--- components/JournalContainer.js
+++ components/JournalContainer.js
@@ -20,32 +20,30 @@ class Journal extends Component{
   }
 
   componentDidMount(props) {
+      function sanitizeFileName(str) {
+        str = str.replace('./', '');
+        str = str.replace('_', ' ');
+        str = str.replace('.txt', '');
+        return str;
+      }
       function importAll(req) {
         let txtfiles = []; // 2D array
         req.keys().map((fileName, index) => {
-          txtfiles.push( [fileName, req(fileName) ] ); });
+          txtfiles.push( [sanitizeFileName(fileName), req(fileName) ] ); });
           return txtfiles;
       }
 
     const req = require.context(JOURNAL_DIR, true, /.txt$/);
     this.setState((state, props) => ({
         journalEntriesArray: importAll(req)
-      }), () => {
-        console.log('journalEntriesArray: ', this.state.journalEntriesArray);
-        // this.renderBody();
-      });
+      }));
     }
     componentDidUpdate(prevProps) {
-      console.log('update: ', prevProps);
       if (prevProps.match.params !== this.props.match.params) {
             const currentEntryId = this.props.match.params.id ? Number(this.props.match.params.id) -1 : null; // minusShifting for array
             this.setState((state, props) => ({
               currentEntryId: currentEntryId
-            }), () => {
-              console.log('STATE UPDATE: ', this);
-              let content = this.determineContent(this.state.currentEntryId, this.state.journalEntriesArray);
-              console.log('NEW CONTENT: ', content);
-            });
+            }));
       }
     }
     determineContent(currentEntryId, journalEntriesArray) {
