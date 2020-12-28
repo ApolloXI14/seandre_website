dd2ef6ca1ae5785dcd58b50cb8d3ea6326174785
Sean McHugh <sean.a.mchugh@gmail.com>
2020-12-28 01:57:23 -0500

    - Made "ImportData" higher-order component to be singular utility for importing data for "WrappedComponent" - Implemented in ImportData "Home" component - .htaccess

--- components/Home.js
+++ components/Home.js
@@ -1,52 +1,42 @@
-// <Link> React components are not handled by "html-react-parser"; consider enhancement?
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import styles from '../less/Home.less'
-import parse from 'html-react-parser';
+import styles from '../less/Home.less';
+import { ImportData } from './ImportData';
+import ReactDOM from 'react-dom';
 
 class Home extends Component{
-	constructor(props) {
+	constructor(props) { 
 		super(props);
 		this.state = {
-			homeEntriesArray: []
+			req: require.context(HOME_DIR, true, /.txt$/)
 		};
 	}
-	componentDidMount(props) {
-		function getFileMetaData(str) { // TODO: Export to own utility later, to de-duplicate
-	        let strArr = str.split('__');
-	        let fileDate = strArr[0];
-	        fileDate = fileDate.replace('./', '');
-	        fileDate =  fileDate.slice(2,4) + '/' + fileDate.slice(4,6) + '/' + fileDate.slice(0,2);
-	        let fileName = strArr[1];
-	        fileName = fileName.replace('.txt', '');
-
-	        
-	        fileName = fileName.replace(/_/g, ' ');
-	        return [fileName, fileDate];
-	      }
-	      function importAll(req) {
-	        let txtfiles = []; // 2D array in [['', ...], ''] form, to get array of metadata (parsed from fileName) and file content
-	        req.keys().map((fileName, index) => {
-	          txtfiles.push( [getFileMetaData(fileName), parse(req(fileName)) ] ); });
-	          return txtfiles;
-	      }
-	      const req = require.context(HOME_DIR, true, /.txt$/);
-	      this.setState((state, props) => ({
-	        homeEntriesArray: importAll(req).reverse() // txt files are titled by date (e.g. "20201231..."), and this makes it reverse chronological
-	      }));
+	componentDidMount(props) { 
+	      const NewComp = (props) => {
+	      	return (
+	      		typeof(props.dataArray) === 'object' && props.dataArray.length && props.dataArray.map((entry, index) => (
+                      <div key={index}>
+                      <div className="entryData-flex">
+                              <div className="entryName">{entry[0][0]}</div><div className="entryDate">Dated: {entry[0][1]}</div>
+                      </div>
+              <div className="entryContent">{entry[1]}</div>
+              <hr/>
+               </div>
+            ))
+	      	)
+	      };
+	      const HomeWithData = ImportData(NewComp, this.state.req);
+	      this.setState((state, props) => ({ 
+	        comp: HomeWithData
+	      }), () => {
+	      	ReactDOM.render(
+	      		<HomeWithData req={this.state.req} />, document.getElementById('homeDiv')
+	      		)
+	      });
 	}
    render(){
       return(
          <div id="homeDiv">
-          	{this.state.homeEntriesArray.length && this.state.homeEntriesArray.map((entry, index) => (
-          		<div key={index}>
-          		<div className="entryData-flex">
-          			<div className="entryName">{entry[0][0]}</div><div className="entryDate">Dated: {entry[0][1]}</div>
-          		</div>
-            	<div className="entryContent">{entry[1]}</div>
-            	<hr/>
-            	</div>
-            ))}
+          	
 		</div>
       );
    }
