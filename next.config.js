1d9eca2aa8c05f29dfc7796ab31c6dd25ab8895a
Sean McHugh <sean.a.mchugh@gmail.com>
2021-07-12 07:38:43 -0400

    Added image loading module to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+const path = require('path');
+
+
 module.exports = {
 	env: {
-		HOME_DIR: '../www/txt/home/'
+		POEMS_DIR: '../public/img/poems/',
+		HOME_DIR: '../public/txt/home/',
+		JOURNAL_DIR: '../public/txt/journal/'
+	},
+	sassOptions: {
+	  includePaths: [path.join(__dirname, 'styles')],
 	},
 	webpack: (config, options) => {
 	    config.module.rules.push({
@@ -11,7 +19,46 @@ module.exports = {
 	          loader: 'file-rawtxt-loader'
 	        },
 	      ],
-	    })
+	    },
+	    {
+           test: /.*\.(gif|png|jpe?g|svg)$/i,
+           use: [
+             {
+               loader: 'file-loader',
+               options: {
+                 name: '[name].[ext]',
+                outputPath: 'static/assets/',
+                publicPath: 'public/img/',
+                 emitFile: true,
+                 byPassOnDebug: true,
+                 disable: true
+               }
+             },
+             {
+              loader: 'image-webpack-loader',
+              options: {
+                mozjpeg: {
+                  progressive: true,
+                },
+                // optipng.enabled: false will disable optipng
+                optipng: {
+                  enabled: false,
+                },
+                pngquant: {
+                  quality: [0.65, 0.90],
+                  speed: 4
+                },
+                gifsicle: {
+                  interlaced: false,
+                },
+                // the webp option will enable WEBP
+                webp: {
+                  quality: 75
+                }
+              }
+            }
+           ]
+         })
         return config;
 	}
 };
