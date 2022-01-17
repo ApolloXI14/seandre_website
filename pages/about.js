0b7a8cafd4f889613a93e1f258cefe4c48c4b71c
Sean McHugh <sean.a.mchugh@gmail.com>
2022-01-17 18:17:18 -0500

    Added emailjs to package.json and implemented in "About" page with EmailJS account values

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import styles from '../styles/about.module.scss';
 import Navbar from '../components/Navbar';
+import emailjs from "emailjs-com";
+import{ init } from '@emailjs/browser';
 
 class About extends Component{
-   render(){
+  componentDidMount(props) {
+    init('user_AoX7PJ11jTjaXT3oOuQVB');
+  }
+  sendMail(e) {
+    console.log('sendMail: ', e.target);
+    event.preventDefault();
+    // generate a five digit number for the contact_number variable
+    //this.contact_number.value = Math.random() * 100000 | 0;
+    // these IDs from the previous steps
+    emailjs.sendForm('contact_service', 'template_9yh8twt', document.getElementById('contact-form'), 'user_AoX7PJ11jTjaXT3oOuQVB')
+        .then(() => {
+            console.log('SUCCESS!');
+        }, function(error) {
+            console.log('FAILED...', error);
+        });
+  }
+  render(){
       return(
         <div id={styles.aboutDiv}>
           <Navbar />
           <h1>About Me</h1>
           	<p>I remember building my first site on angelfire as a 90s kid, to share
+  }
 my writing. It has been lost to time sadly, but that's why I made this
 one! Using ReactJS/less instead of angelfire, though.  ;)</p>
             <p>Whatever brought you here, thank you. I'm but a humble
@@ -25,6 +44,19 @@ one! Using ReactJS/less instead of angelfire, though.  ;)</p>
         <li>Favorite flavor: Chocolate</li>
         <li>Favorite movie: Good Will Hunting</li>
       </ul>
+
+      <form id="contact-form">
+        <input type="hidden" name="contact_number" value={Math.random() * 100000 | 0}/>
+        <label>Name</label>
+        <input type="text" name="user_name"/>
+        <label>Email</label>
+        <input type="email" name="user_email"/>
+        <label>Message</label>
+        <textarea name="message"></textarea>
+        <input type="submit" value="Send" onClick={this.sendMail.bind()}/>
+        <button type="button" onClick={this.sendMail.bind()}>Send Button</button>
+    </form>
+
 		</div>
       );
    }
