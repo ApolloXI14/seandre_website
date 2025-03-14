747b5d3f9a4d0e4566ff566ab3ce2386e28c9691
Sean McHugh <sean.a.mchugh@gmail.com>
2025-03-14 19:35:12 -0400

    Made useEffect hook to ensure Recaptcha script tag API always runs so captcha renders

--- components/Recaptcha.js
+++ components/Recaptcha.js
@@ -6,10 +6,19 @@ function Recaptcha({recaptchaKey}) {
 	  useEffect(() => {
 	    setIsClient(true)
 	  }, [])
+	  useEffect(() => {
+	  const script = document.createElement('script');
+	  script.src = "https://www.google.com/recaptcha/enterprise.js";
+	  script.async = true;
+	  script.defer = true;
+	  document.body.appendChild(script);
+		return () => {
+				document.body.removeChild(script);
+			}
+	}, []);
 	return ( 
 		<div>
 			<div className={styles.recaptchaDiv}>
-		        <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 				{isClient ? 'Never prerendered' :
 					<div id="recaptcha" className="g-recaptcha" data-sitekey={recaptchaKey}></div>
 				}
