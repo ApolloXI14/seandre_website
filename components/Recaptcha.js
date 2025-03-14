c1cb57943cea60f9aa136e3c74c57d3f0d265da5
Sean McHugh <sean.a.mchugh@gmail.com>
2025-03-13 20:37:03 -0400

    Restoring useEffect

--- components/Recaptcha.js
+++ components/Recaptcha.js
@@ -2,12 +2,17 @@ import React, { useState, useEffect } from 'react';
 import styles from '../styles/about.module.scss';
 
 function Recaptcha({recaptchaKey}) {
+	const [isClient, setIsClient] = useState(false);
+	  useEffect(() => {
+	    setIsClient(true)
+	  }, [])
 	return ( 
 		<div>
 			<div className={styles.recaptchaDiv}>
 		        <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-		        <div id="recaptcha" className="g-recaptcha" data-sitekey={recaptchaKey}></div>
-
+				{isClient ? 'Never prerendered' :
+					<div id="recaptcha" className="g-recaptcha" data-sitekey={recaptchaKey}></div>
+				}
 	    	</div>
 			 
 	    </div>
