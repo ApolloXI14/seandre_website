598a28a1db8d35ef984fb63a9d193fa631ed93b2
Sean McHugh <sean.a.mchugh@gmail.com>
2025-03-19 09:36:24 -0400

    Integrated API response into pages/journal/index HTML and removed txt file require and all references

--- pages/journal/index.js
+++ pages/journal/index.js
@@ -6,41 +6,15 @@ import styles from '../../styles/journalmenu.module.scss';
 import axios from "axios";
 
 export default function Journal() {
-    const getFileMetaData = (str)=> {
-        let strArr = str.split('__');
-        let fileDate = strArr[0];
-        fileDate = fileDate.replace('./', '');
-        fileDate =  fileDate.slice(2,4) + '/' + fileDate.slice(4,6) + '/' + fileDate.slice(0,2);
-        let fileName = strArr[1];
-        fileName = fileName.replace('.txt', '');
-        fileName = fileName.replace(/_/g, ' ');
-        return [fileName, fileDate];
-    }
-
     const [fileNameArray, setArray] = useState([]);
-
-    const [newFileArray, setNewArray] = useState([]);
-
     useEffect( ()=> {
-
         axios.get("http://localhost:5000/journals")
           .then(response => {
             console.log('TEST: ', response.data);
-            setNewArray(response.data)
+            setArray(response.data)
           })
           .catch(error => console.error(error));
-
-        console.log(newFileArray);
-
-        const reqVar = require.context(process.env.JOURNAL_DIR, true, /.txt$/);
-        let files = [];
-        reqVar.keys().map((item, index) => {
-            files.push(getFileMetaData(item.replace('./', '')));
-        });
-        setArray(files.reverse())
-        console.log('OLD: ', fileNameArray);
     }, []);
-
     return (
         <div>
           <Navbar />
@@ -48,7 +22,8 @@ export default function Journal() {
             <ul>
               <ul className={styles.listClass}>
                 {fileNameArray.map((entry, index, array) => (
-                  <li key={index++}><Link href={`journal/${array.length - index}`}>{entry[0]}</Link> - <cite>Published {entry[1]}</cite></li>
+                  <li key={index++}><Link href={`journal/${array.length - index}`}>
+                  {entry.title}</Link> - <cite>Published {entry.date.toString().slice(2,4) + '/' + entry.date.toString().slice(4,6) + '/' + entry.date.toString().slice(0,2)}</cite></li>
                   ))}
               </ul>
             </ul>
