a0f285d87a259126be4f5743190d659e3cac3248
Sean McHugh <samchugh@proton.me>
2025-03-26 03:11:21 -0400

    Fix

--- app/page.js
+++ app/page.js
@@ -10,9 +10,8 @@ export const metadata = {
 
 export default async function App() {
     const homeArray = await fetch("http://localhost:5000/homes", { next: { revalidate: 3600 }})
-		.then(response => {
-            return response.json();
-        }).catch(error => console.error(error));
+		.then(response => response.json()
+        ).catch(error => console.error(error));
 	return (
         <div>
           <Navbar />
