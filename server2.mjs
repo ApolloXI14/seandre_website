249a856f4db8a67a302cb77908e3ad8e14bf9eea
Sean McHugh <samchugh@proton.me>
2025-08-15 09:34:57 -0400

    dotenv fix in node server2.mjs

--- server2.mjs
+++ server2.mjs
@@ -1,4 +1,4 @@
-// require("dotenv").config();
+require("dotenv").config();
 import { MongoClient } from "mongodb";
 const connectionString = process.env.MONGODB_URI || "";
 const client = new MongoClient(connectionString);
