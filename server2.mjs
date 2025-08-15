b9d133434d0b188038d371f4ad52b5beeb8f3993
root <samchugh@proton.me>
2025-08-14 21:03:22 -0400

    Added "undefined" check for db in node server to fix remote connection issues

--- server2.mjs
+++ server2.mjs
@@ -1,12 +1,28 @@
+// require("dotenv").config();
 import { MongoClient } from "mongodb";
 const connectionString = process.env.MONGODB_URI || "";
 const client = new MongoClient(connectionString);
 let conn;
-try {
+let db;
+
+export async function makeMongoDBConnection() {
   conn = await client.connect();
-  console.log('MongoDB connection successful');
+  return conn.db("journal");
+}
+
+try {
+  // conn = await client.connect();
+  // db =  conn.db("journal");
+
+  if (typeof(db) === 'undefined') {
+    db = await makeMongoDBConnection();
+    console.log('MongoDB connection successful');
+  } else {
+    console.log('Already connected')
+  }
+
 } catch(e) {
   console.error(e);
 }
-let db = conn.db("journal");
+//let db = conn.db("journal");
 export default db;
