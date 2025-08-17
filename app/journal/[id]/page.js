bd615df0be31475e666f2c310340cb1173c6a220
root <samchugh@proton.me>
2025-08-17 04:11:51 -0400

    Added "getJournalEntry" back to "getStaticParams" in journal/[id]/page to fix remote 404s

--- app/journal/[id]/page.js
+++ app/journal/[id]/page.js
@@ -5,22 +5,11 @@ import JournalEntryComp from "../../../components/JournalEntry";
 import Navbar from "../../../components/Navbar";
 import { getJournalEntry } from '../../../serverFunctions.js';
 
+// const dynamic = 'force-static'
 
-// async function getJournalEntry( title ) {
-//    return await fetch(process.env.DB_HOST + ':' + process.env.DB_PORT + '/api/' + (title ?  'journals/' + title : 'journals'), { next: { revalidate: 3600 }})
-// // const res = await fetch(process.env.DB_HOST + ':' + process.env.DB_PORT + '/' + 'journals', { next: { revalidate: 3600 }})
-// 		.then(response => {
-//           if (response.status === 200) {
-//             return response.json()
-//           }
-//         }).catch(error => console.error(error));
-// }
-
-// TODO: Move DB call back into "generateStaticParams" if this does not work
-const dynamic = 'force-static'
-
-export async function generateStaticParams({params}) {
-  return (Array.isArray(params) && params || []).map( (journal, index) => {
+export async function generateStaticParams() {
+  const res = await getJournalEntry();
+  return (Array.isArray(res) && res || []).map( (journal, index) => {
     { id: journal.title?.replaceAll(" ", "-").toLowerCase() }
   })
 }
