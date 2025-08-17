0d84a925be14b011e4416abd4a4926dab17bc8b3
root <samchugh@proton.me>
2025-08-17 04:19:57 -0400

    fix attempt

--- app/journal/[id]/page.js
+++ app/journal/[id]/page.js
@@ -5,7 +5,9 @@ import JournalEntryComp from "../../../components/JournalEntry";
 import Navbar from "../../../components/Navbar";
 import { getJournalEntry } from '../../../serverFunctions.js';
 
-// const dynamic = 'force-static'
+
+const dynamic = 'force-static'
+const dynamicParams = false;
 
 export async function generateStaticParams() {
   const res = await getJournalEntry();
