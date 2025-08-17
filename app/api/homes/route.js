f90da02f9cbbe7f59e129019d122b443c09f861b
root <samchugh@proton.me>
2025-08-16 23:17:53 -0400

    Push

--- app/api/homes/route.js
+++ app/api/homes/route.js
@@ -1,9 +1,19 @@
 //import { app } from '../server.js';
 require('dotenv').config()
-import  db  from '../../../server2.mjs'
+// import  db  from '../../../server2.mjs'
+import  {makeMongoDBConnection}  from '../../../server3.js'
+
+// (async function(){
+//
+//   var foo = await require('../../../server3.js');
+//   console.log(foo);
+// })();
+
 
 export const dynamic = 'force-static';
 export async function GET() {
+  const db = await makeMongoDBConnection();
+
   const journalCollection = db.collection("homes");
   async function getHome() {
           return await journalCollection.find({}, {"title": true, "date": true, "content": true}).sort({date: -1}).toArray();
