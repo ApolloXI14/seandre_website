54b3f65ef8960c5d7b68c1f8fc74946fefa48eb7
Sean McHugh <samchugh@proton.me>
2025-08-21 18:48:00 -0400

    - Made ".mongoshrc" file to execute when server runs and run updates - Made "server/createJournalsView.js" server update - Updated "html-react-parser" in package.json

---
+++ .mongoshrc.js
@@ -0,0 +1,9 @@
+// https://www.mongodb.com/docs/mongodb-shell/write-scripts/#execute-mongodb-code
+
+require("dotenv").config();
+db = connect(process.env.MONGODB_URI);
+
+db.clientConnections.insertOne( { connectTime: ISODate() } )
+
+// Load DB scripts to be executed
+load('server/createJournalsView.js')
