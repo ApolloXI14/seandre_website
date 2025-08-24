c01979abdf57acc8ada30f7e732cb221e9957861
Sean McHugh <samchugh@proton.me>
2025-08-24 02:27:23 -0400

    New view user role for system.views to unblock making dumps

--- server/RUN_ORDER.js
+++ server/RUN_ORDER.js
@@ -1,4 +1,4 @@
-const scriptArray = ['fixHomeLinks.js', 'createStoriesCollection.js', 'updateActiveViews.js', 'homesUpdate.js']
+const scriptArray = ['fixHomeLinks.js', 'createStoriesCollection.js', 'updateActiveViews.js', 'homesUpdate.js', 'createViewUserRole.js']
 
 for (script in scriptArray) {
 	load(script);
