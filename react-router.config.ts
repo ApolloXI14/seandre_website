7d4ca53d0aaff634189f713ca162c75f17c0895a
Sean McHugh <samchugh@proton.me>
2025-10-09 23:18:40 -0400

    - Created prerender route logic in "react-router.config.ts"

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "@react-router/dev/config";
+import { getViews, getDocuments } from 'app/serverFunctions.ts';
 
 export default {
   // Config options...
@@ -6,10 +7,88 @@ export default {
   future: {
     v8_middleware: true,
   },
-  appDirectory: "app",
-  ssr: true,
-  prerender: async ({ getStaticPaths }) => {
-    const paths = await getStaticPaths();
-    return ["/", ...paths];
+  ssr: false,
+  async prerender({ getStaticPaths }) {
+    let realPathsArray = [];
+    let activeViewsArray = [];
+    //const collectionNameArray = ["homes", "poems", "stories"];
+    let res = await getViews();
+    // activeViewsArray = res.map( (view) => {
+    //   return "/" + view._id;
+    // })
+    //   // .then( (val) => {
+    //   //   console.log('test: ', val)
+    //   // } )
+    // console.log('activeViewsArray: ', activeViewsArray);
+    // let res2 = ["poems", "stories", "journals"].map( async (collectionName) => {
+    //   async function getViewDocs(viewName) {
+    //     return await getDocuments(viewName);
+    //   }
+    //   // console.log('collectionName: ', await getViewDocs(collectionName))
+    //   return await getViewDocs(collectionName);
+    //   // console.log('test: ', res[0]);
+    //   // console.log('res: ', res)
+    //   // let resArray = []
+    //   // resArray = res.map( (item) => {
+    //   //   // console.log('??: ', item)
+    //   //   return "/" + item.realPath;
+    //   // });
+    //   // realPathsArray = [...realPathsArray, ...resArray];
+    // //   console.log('realPathsArray: ', realPathsArray)
+    // //   // return resArray;
+    // });
+
+    // console.log('realPathsArray final: ', realPathsArray)
+
+//   // TODO: Fix the dynamic method above to replace this hardcoded logic
+    let poemsRealPaths = [];
+    const poemsPathArray = await getDocuments('poems');
+    poemsRealPaths = poemsPathArray.map( (item) => {
+      return "/" + item.realPath;
+    });
+
+    let storiesRealPaths = [];
+    const storiesPathArray = await getDocuments('stories');
+    storiesRealPaths = storiesPathArray.map( (item) => {
+      return "/" + item.realPath;
+    });
+
+    let journalsRealPaths = [];
+    const journalsPathArray = await getDocuments('journals');
+    journalsRealPaths = journalsPathArray.map( (item) => {
+      return "/" + item.realPath;
+    });
+    // console.log('res2: ', res2)
+
+
+    // return ["/",
+    //         "/poems", ...poemsRealPaths,
+    //         "/stories", ...storiesRealPaths,
+    //         "/journals", ...journalsPathArray];
+
+        // return ["/",
+        //     ...activeViewsArray];
+
+    // console.log('???: ', ...res2.map( (item) => {
+    //       return "/" + item.realPath;
+    //     }))
+
+    return ["/",
+      ...res.map( (view) => {
+          return "/" + view._id;
+        }),
+        ...poemsRealPaths,
+        ...storiesRealPaths,
+        ...journalsRealPaths
+           ]
+
+
+
   },
 } satisfies Config;
+
+
+
+// prerender: async ({ getStaticPaths }) => {
+//     return ["/", "/poems", ":collectionName/", ":collectionName/:id"];
+//   },
--- vite.config.ts
+++ vite.config.ts
@@ -8,68 +8,75 @@
 import { reactRouter } from "@react-router/dev/vite";
 import autoprefixer from "autoprefixer";
 import tailwindcss from "tailwindcss";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import netlifyPlugin from "@netlify/vite-plugin-react-router";
 import { reactRouterDevTools } from "react-router-devtools";
 import { VitePluginNode } from 'vite-plugin-node';
 
 export default defineConfig({
+  //   server: {
+  //   // vite server configs, for details see [vite doc](https://vitejs.dev/config/#server-host)
+  //   port: 3000
+  // },
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
     },
   },
-  plugins: [reactRouterDevTools(), reactRouter(), tsconfigPaths(), netlifyPlugin(),
-//       VitePluginNode({
-//       // Nodejs native Request adapter
-//       // currently this plugin support 'express', 'nest', 'koa' and 'fastify' out of box,
-//       // you can also pass a function if you are using other frameworks, see Custom Adapter section
-//
-//       // adapter({ app, server, req, res, next }) {
-//       //   app(res, res);
-//       // },
-//
-//       adapter: 'express',
-//
-//       // tell the plugin where is your project entry
-//       appPath: './app/root.tsx',
-//
-//       // Optional, default: 'viteNodeApp'
-//       // the name of named export of you app from the appPath file
-//       exportName: 'viteNodeApp',
-//
-//       // Optional, default: false
-//       // if you want to init your app on boot, set this to true
-//       initAppOnBoot: false,
-//
-//       // Optional, default: false
-//       // if you want to reload your app on file changes, set this to true, rebounce delay is 500ms
-//       reloadAppOnFileChange: false,
-//
-//       // Optional, default: 'esbuild'
-//       // The TypeScript compiler you want to use
-//       // by default this plugin is using vite default ts compiler which is esbuild
-//       // 'swc' compiler is supported to use as well for frameworks
-//       // like Nestjs (esbuild dont support 'emitDecoratorMetadata' yet)
-//       // you need to INSTALL `@swc/core` as dev dependency if you want to use swc
-//       tsCompiler: 'esbuild',
-//
-//       // Optional, default: {
-//       // jsc: {
-//       //   target: 'es2019',
-//       //   parser: {
-//       //     syntax: 'typescript',
-//       //     decorators: true
-//       //   },
-//       //  transform: {
-//       //     legacyDecorator: true,
-//       //     decoratorMetadata: true
-//       //   }
-//       // }
-//       // }
-//       // swc configs, see [swc doc](https://swc.rs/docs/configuration/swcrc)
-//       swcOptions: {}
-//     })
+  plugins: [reactRouterDevTools(), reactRouter(), tsconfigPaths(),
+      netlifyPlugin(),
+
+    //   VitePluginNode({
+    //   // Nodejs native Request adapter
+    //   // currently this plugin support 'express', 'nest', 'koa' and 'fastify' out of box,
+    //   // you can also pass a function if you are using other frameworks, see Custom Adapter section
+    //
+    //   // adapter({ app, server, req, res, next }) {
+    //   //   app(res, res);
+    //   // },
+    //
+    //   adapter: 'express',
+    //
+    //   // tell the plugin where is your project entry
+    //   // appPath: './app/root.tsx',
+    //   appPath: './app/app.server.ts',
+    //
+    //   // Optional, default: 'viteNodeApp'
+    //   // the name of named export of you app from the appPath file
+    //   exportName: 'viteNodeApp',
+    //
+    //   // Optional, default: false
+    //   // if you want to init your app on boot, set this to true
+    //   initAppOnBoot: false,
+    //
+    //   // Optional, default: false
+    //   // if you want to reload your app on file changes, set this to true, rebounce delay is 500ms
+    //   reloadAppOnFileChange: false,
+    //
+    //   // Optional, default: 'esbuild'
+    //   // The TypeScript compiler you want to use
+    //   // by default this plugin is using vite default ts compiler which is esbuild
+    //   // 'swc' compiler is supported to use as well for frameworks
+    //   // like Nestjs (esbuild dont support 'emitDecoratorMetadata' yet)
+    //   // you need to INSTALL `@swc/core` as dev dependency if you want to use swc
+    //   tsCompiler: 'esbuild',
+    //
+    //   // Optional, default: {
+    //   // jsc: {
+    //   //   target: 'es2019',
+    //   //   parser: {
+    //   //     syntax: 'typescript',
+    //   //     decorators: true
+    //   //   },
+    //   //  transform: {
+    //   //     legacyDecorator: true,
+    //   //     decoratorMetadata: true
+    //   //   }
+    //   // }
+    //   // }
+    //   // swc configs, see [swc doc](https://swc.rs/docs/configuration/swcrc)
+    //   swcOptions: {}
+    // })
   ],
 });
