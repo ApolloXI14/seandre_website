75785fc17995dfb84f31119a88608b1d4f792bb6
Sean McHugh <samchugh@proton.me>
2025-12-16 01:24:12 -0500

    tsconfig fix and package.json updates

--- components/Home.tsx
+++ components/Home.tsx
@@ -13,14 +13,18 @@ interface Entry {
 	title: string;
 	date: number;
 	content: string;
-	baseCase: string;
-	realpath: string;
+	baseCase?: string;
+	realpath?: string;
 }
+/*
+interface EntryArray extends Entry {
+	[index: number | string]: any
+}*/
 
 export default function HomeComp({homeArray}: {homeArray: Array<Entry>[]}) {
     return (
 		<div id="homeDiv">
-			{homeArray.map( (entry : Entry, index: number) => (
+// 			{homeArray.map( (entry : any, index: number) => (
 				<div className={styles.entryDiv} key={index}>
 				<div className={styles['entryData-flex']}>
 						<div className={styles.entryName}>{entry.title}</div><div className={styles.entryDate}>Dated: {entry.date.toString().slice(2,4) + '/' + entry.date.toString().slice(4,6) + '/' + entry.date.toString().slice(0,2)}</div>
