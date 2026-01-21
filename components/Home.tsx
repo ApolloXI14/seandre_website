633ee2cd2c72e19b174671219630c86c727284e6
Sean McHugh <samchugh@proton.me>
2026-01-20 21:06:14 -0500

    Fixed text

--- components/Home.tsx
+++ components/Home.tsx
@@ -24,7 +24,7 @@ interface EntryArray extends Entry {
 export default function HomeComp({homeArray}: {homeArray: Array<Entry>[]}) {
     return (
 		<div id="homeDiv">
-// 			{homeArray.map( (entry : any, index: number) => (
+ 			{homeArray.map( (entry : any, index: number) => (
 				<div className={styles.entryDiv} key={index}>
 				<div className={styles['entryData-flex']}>
 						<div className={styles.entryName}>{entry.title}</div><div className={styles.entryDate}>Dated: {entry.date.toString().slice(2,4) + '/' + entry.date.toString().slice(4,6) + '/' + entry.date.toString().slice(0,2)}</div>
